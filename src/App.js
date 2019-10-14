3cae88c4a4fbd299286795d6954252a83ea88c74
Chris Martin <chris.bizunit34@gmail.com>
2019-10-13 20:29:21 -0400

    updated the styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import LandingPage from './LandingPage/LandingPage';
 import ProjectPage from './ProjectPage/ProjectPage';
 import AboutMePage from './AboutMePage/AboutMePage';
 import Header from './Header/Header';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
+
+library.add(fab)
 
 class App extends React.Component {
   render(){
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 function Header() {
@@ -13,15 +15,18 @@ function Header() {
             </Link>
           </p>
       </div>
+      
+
+      
       <DropdownButton size='sm' variant='secondary' className='navigation-list' id='dropdown-basic-button' title='Navigation'>
         <Dropdown.Item href='/'>Home</Dropdown.Item>
         <Dropdown.Item href='/aboutme'>About Me</Dropdown.Item>
         <Dropdown.Item href='/projects'>Projects</Dropdown.Item>
       </DropdownButton>
       <DropdownButton size='sm' variant='secondary' className='contact-list' id='dropdown-basic-button' title='Contact Me'>
-        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/chrisdmartin-18/'>LinkedIn</Dropdown.Item>
-        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='https://github.com/bizunit34'>GitHub</Dropdown.Item>
-        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='mailto:chris.bizunit34@gmail.com'>Email</Dropdown.Item>
+        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/chrisdmartin-18/'><FontAwesomeIcon className='icon-link' color='black' icon={['fab', 'linkedin']} /> LinkedIn</Dropdown.Item>
+        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='https://github.com/bizunit34'><FontAwesomeIcon className='icon-link' color='black' icon={['fab', 'github']} /> GitHub</Dropdown.Item>
+        <Dropdown.Item target='_blank' rel='noopener noreferrer' href='mailto:chris.bizunit34@gmail.com'><FontAwesomeIcon className='icon-link' color='black' icon={faEnvelopeSquare} /> Email</Dropdown.Item>
       </DropdownButton>
     </>
   )
